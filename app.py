06dcea29911a504338ecfcb6e1259920a3083448
Roberto Novaes <rnovaes@ufmg.br>
2021-03-16 16:45:24 -0300

    Corrigida a formatacao com Black. Removidos imports desnecessarios

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from gestaolegaldaj import app
 from flask_wtf.csrf import CSRFProtect
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from sqlalchemy import  text
 
 csrf = CSRFProtect(app)
 
-if __name__ == '__main__':
-	app.run(debug=False)
-	csrf.init_app(app)
+if __name__ == "__main__":
+    app.run(debug=False)
+    csrf.init_app(app)
