e92fdc63dd1a64cdaad15ca906aaa60c3738d596
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2021-10-14 16:45:24 -0300

    121 Correcao de var em js e melhoria do if

--- gestaolegal/static/js/subarea_caso.js
+++ gestaolegal/static/js/subarea_caso.js
@@ -1,22 +1,7 @@
-function validarCampoAreaDireito() {
-    var elemento_selecionado = document.getElementById('area_direito-js');
-    var string_selecionada = elemento_selecionado.options[elemento_selecionado.selectedIndex].text;
+function validarCampoAreaDireito()  {
+    let elemento_selecionado = document.getElementById('area_direito-js');
+    let string_selecionada = elemento_selecionado.options[elemento_selecionado.selectedIndex].text;
 
-    if(string_selecionada == "Civel"){
-
-        document.getElementById("div_sub_area").hidden = false;
-
-    }
-    else{
-
-        document.getElementById("div_sub_area").hidden = true;
-    }
-    if(string_selecionada == "Administrativo"){
-
-        document.getElementById("div_sub_area_admin").hidden = false;
-
-    }else{
-
-        document.getElementById("div_sub_area_admin").hidden = true;
-    }
+    document.getElementById("div_sub_area").hidden = string_selecionada !== "Civel";
+    document.getElementById("div_sub_area_admin").hidden = string_selecionada !== "Administrativo";
 }
