4ee675d3942f227eef062edf3ed280922b6fbd7d
davi-v <davibtv@gmail.com>
2022-07-12 17:09:38 -0300

    Arquivo obrigatório em "Arquivos"

--- gestaolegal/arquivos/views.py
+++ gestaolegal/arquivos/views.py
@@ -15,79 +15,84 @@ arquivos = Blueprint('arquivos', __name__, template_folder='templates')
 @arquivos.route('/')
 @login_required()
 def index():
-    page = request.args.get('page', 1, type=int)
-    arquivos = Arquivo.query.paginate(page, app.config['ARQUIVOS_POR_PAGINA'], False)
-    return render_template('arquivos.html', arquivos = arquivos)
+	page = request.args.get('page', 1, type=int)
+	arquivos = Arquivo.query.paginate(page, app.config['ARQUIVOS_POR_PAGINA'], False)
+	return render_template('arquivos.html', arquivos = arquivos)
 
 @arquivos.route('/cadastrar_arquivo', methods = ['GET','POST'])
-@login_required(role=[usuario_urole_roles['ADMINISTRADOR'][0], usuario_urole_roles['PROFESSOR'][0], usuario_urole_roles['COLAB_PROJETO'][0], usuario_urole_roles['COLAB_EXTERNO'][0]])
+@login_required(role=[
+	usuario_urole_roles['ADMINISTRADOR'][0],
+	usuario_urole_roles['PROFESSOR'][0],
+	usuario_urole_roles['COLAB_PROJETO'][0],
+	usuario_urole_roles['COLAB_EXTERNO'][0]
+	])
 def cadastrar_arquivo():
-    _form = ArquivoForm()
-    if _form.validate_on_submit():
-        arquivo = request.files.get(_form.arquivo.name)
-        if not arquivo:
-        	flash('Você precisa adicionar um arquivo.','warning')
-        	return redirect(url_for('arquivos.cadastrar_arquivo'))
-        _arquivo = Arquivo(
-            titulo = _form.titulo.data,
-            descricao = _form.descricao.data,
-            nome = arquivo.filename
-        )
-        arquivo.save(os.path.join(current_app.root_path,'static','arquivos', arquivo.filename))
-        db.session.add(_arquivo)
-        db.session.commit()
-        flash('Arquivo adicionado','success')
-        return redirect(url_for('arquivos.index'))
+	_form = ArquivoForm()
+	if _form.validate_on_submit():
+		arquivo = request.files.get(_form.arquivo.name)
+		if not arquivo:
+			flash('Você precisa adicionar um arquivo.','warning')
+			return redirect(url_for('arquivos.cadastrar_arquivo'))
+		_arquivo = Arquivo(
+			titulo = _form.titulo.data,
+			descricao = _form.descricao.data,
+			nome = arquivo.filename
+		)
+		arquivo.save(os.path.join(current_app.root_path,'static','arquivos', arquivo.filename))
+		db.session.add(_arquivo)
+		db.session.commit()
+		flash('Arquivo adicionado','success')
+		return redirect(url_for('arquivos.index'))
 
-    return render_template('cadastrar_arquivo.html', form = _form)
+	return render_template('cadastrar_arquivo.html', form = _form)
 
 @arquivos.route('/editar_arquivo/<int:id>', methods = ['GET','POST'])
 @login_required(role=[usuario_urole_roles['ADMINISTRADOR'][0], usuario_urole_roles['PROFESSOR'][0], usuario_urole_roles['COLAB_PROJETO'][0], usuario_urole_roles['COLAB_EXTERNO'][0]])
 def editar_arquivo(id):
-    _arquivo = Arquivo.query.get_or_404(id)
-    _form = ArquivoForm()
+	_arquivo = Arquivo.query.get_or_404(id)
+	_form = ArquivoForm()
 
-    if _form.validate_on_submit():
-        arquivo = request.files.get(_form.arquivo.name)
-        _arquivo.titulo = _form.titulo.data,
-        _arquivo.descricao = _form.descricao.data
+	if _form.validate_on_submit():
+		arquivo = request.files.get(_form.arquivo.name)
+		_arquivo.titulo = _form.titulo.data,
+		_arquivo.descricao = _form.descricao.data
 
-        if arquivo.filename:
-            local_arquivo = os.path.join(current_app.root_path,'static','arquivos', _arquivo.nome)
-            if os.path.exists(local_arquivo):
-                os.remove(local_arquivo)
-            
-            _arquivo.nome = arquivo.filename
-            arquivo.save(os.path.join(current_app.root_path,'static','arquivos', arquivo.filename))
+		if arquivo.filename:
+			local_arquivo = os.path.join(current_app.root_path,'static','arquivos', _arquivo.nome)
+			if os.path.exists(local_arquivo):
+				os.remove(local_arquivo)
+			
+			_arquivo.nome = arquivo.filename
+			arquivo.save(os.path.join(current_app.root_path,'static','arquivos', arquivo.filename))
 
-        db.session.commit()
-        flash('Arquivo editado','success')
-        return redirect(url_for('arquivos.visualizar_arquivo', id = id))
+		db.session.commit()
+		flash('Arquivo editado','success')
+		return redirect(url_for('arquivos.visualizar_arquivo', id = id))
 
-    _form.titulo.data = _arquivo.titulo
-    _form.descricao.data = _arquivo.descricao
+	_form.titulo.data = _arquivo.titulo
+	_form.descricao.data = _arquivo.descricao
 
-    return render_template('editar_arquivo.html', form = _form)
+	return render_template('editar_arquivo.html', form = _form)
 
 
 @arquivos.route('/visualizar_arquivo/<int:id>')
 @login_required()
 def visualizar_arquivo(id):
-    _arquivo = Arquivo.query.get_or_404(id)
-    return render_template('visualizar_arquivo.html', arquivo = _arquivo)
+	_arquivo = Arquivo.query.get_or_404(id)
+	return render_template('visualizar_arquivo.html', arquivo = _arquivo)
 
 @arquivos.route('/excluir_arquivo/<int:id>')
 @login_required(role=[usuario_urole_roles['ADMINISTRADOR'][0], usuario_urole_roles['PROFESSOR'][0], usuario_urole_roles['COLAB_PROJETO'][0]])
 def excluir_arquivo(id):
-    _arquivo = Arquivo.query.get_or_404(id)
-    local_arquivo = os.path.join(current_app.root_path,'static','arquivos', _arquivo.nome)
+	_arquivo = Arquivo.query.get_or_404(id)
+	local_arquivo = os.path.join(current_app.root_path,'static','arquivos', _arquivo.nome)
 
-    if os.path.exists(local_arquivo):
-        os.remove(local_arquivo)
-    
-    db.session.delete(_arquivo)
-    db.session.commit()
+	if os.path.exists(local_arquivo):
+		os.remove(local_arquivo)
+	
+	db.session.delete(_arquivo)
+	db.session.commit()
 
-    flash('arquivo excluido.')
+	flash('arquivo excluido.')
 
-    return redirect(url_for('arquivos.index'))
+	return redirect(url_for('arquivos.index'))
