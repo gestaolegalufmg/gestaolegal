53758fa5731ae38a2dc5f881a462ac89108f16d7
marcelomrad <marcelomrad@gmail.com>
2022-07-21 14:35:46 -0300

    fix: add filtro indeferido/ aguardand meus casos

--- gestaolegal/casos/views_utils.py
+++ gestaolegal/casos/views_utils.py
@@ -18,6 +18,10 @@ opcoes_filtro_meus_casos = {
 }
 opcoes_filtro_meus_casos['ATIVO'] = situacao_deferimento['ATIVO']
 opcoes_filtro_meus_casos['ARQUIVADO'] = situacao_deferimento['ARQUIVADO']
+opcoes_filtro_meus_casos['AGUARDANDO_DEFERIMENTO'] = situacao_deferimento['AGUARDANDO_DEFERIMENTO']
+opcoes_filtro_meus_casos['INDEFERIDO'] = situacao_deferimento['INDEFERIDO']
+
+
 
 opcoes_filtro_eventos = tipo_evento
 opcoes_filtro_eventos['TODOS'] = ('todos', 'Todos')
