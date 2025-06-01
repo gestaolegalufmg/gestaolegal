cbd840ed375a4fcc0e0bdef9393be205d2266b4b
AndreOGodoy <godoy.andre@hotmail.com>
2025-06-01 18:47:00 -0300

    chore: simplify project environments

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t test-runner -f tests/Dockerfile.test .
-docker run -it --rm --init --ipc=host --network=development_gestaolegal test-runner
+docker run -it --rm --init --ipc=host --network=gestaolegal_default test-runner
