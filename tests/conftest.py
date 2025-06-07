5e5430439c0c5cf45a2f4bd81e94325f502542f1
AndreOGodoy <godoy.andre@hotmail.com>
2025-06-07 00:11:23 -0300

    chore: fix bugs + improve tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
-import pytest
+import os
 
+import pytest
 
 @pytest.fixture(scope="session")
 def browser_context_with_auth(browser, base_url: str):
@@ -7,23 +8,25 @@ def browser_context_with_auth(browser, base_url: str):
     page = context.new_page()
 
     page.goto("/usuario/login")
-    page.locator("#login").click()
     page.locator("#login").fill("admin@gl.com")
-    page.locator("#login").press("Tab")
     page.locator("#senha").fill("123456")
     page.get_by_role("button", name="Login").click()
 
-    context.storage_state(path="./auth_state.json")
+    page.wait_for_url("/")
 
-    yield context
+    auth_file = "./auth_state.json"
+    context.storage_state(path=auth_file)
+
     context.close()
+    yield context
+
+    if os.path.exists(auth_file):
+        os.remove(auth_file)
 
 
 @pytest.fixture
-def auth_page(browser, base_url: str):
+def auth_page(browser, browser_context_with_auth, base_url: str):
     context = browser.new_context(storage_state="./auth_state.json", base_url=base_url)
     page = context.new_page()
-
     yield page
-
     context.close()
---
+++ tests/test_files.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, expect
+
+
+# TODO(Andre): Check the full form structure
+def test_file_register_page_title(auth_page: Page) -> None:
+    auth_page.goto("/arquivos/cadastrar_arquivo")
+
+    # TODO(Andre): Understand why get_by_label isn't working as expect
+    expect(auth_page.get_by_text('Título *', exact=True)).to_be_visible();
