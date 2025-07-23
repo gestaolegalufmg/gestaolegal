81e54edfed984563c9d4f9393605d4a7c60ec39d
AndreOGodoy <godoy.andre@hotmail.com>
2025-07-23 13:07:46 -0300

    feat: busca_geral_orientacoes

--- gestaolegal/principal/views.py
+++ gestaolegal/principal/views.py
@@ -4,7 +4,12 @@ from sqlalchemy import and_, or_
 from gestaolegal import app, db, login_required
 from gestaolegal.casos.models import Caso, associacao_casos_atendidos
 from gestaolegal.models.atendido import Atendido
-from gestaolegal.plantao.models import Assistido, AssistidoPessoaJuridica
+from gestaolegal.models.orientacao_juridica import OrientacaoJuridica
+from gestaolegal.plantao.models import (
+    Assistido,
+    AssistidoPessoaJuridica,
+    Atendido_xOrientacaoJuridica,
+)
 from gestaolegal.usuario.models import Usuario
 
 principal = Blueprint("principal", __name__, template_folder="templates")
@@ -122,6 +127,8 @@ def busca_geral():
     )
 
     casos = None
+    orientacoes_juridicas = None
+
     if busca.isdigit():
         casos_stmt = select(Caso).where(
             and_(Caso.status == True, Caso.id == int(busca))
@@ -139,11 +146,26 @@ def busca_geral():
         )
         casos = create_pagination(casos_stmt, page_caso, app.config["CASOS_POR_PAGINA"])
 
+        # Busca orientações jurídicas que tenham atendidos com nome similar ao termo de busca
+        orientacoes_stmt = (
+            select(OrientacaoJuridica)
+            .join(Atendido_xOrientacaoJuridica)
+            .join(Atendido)
+            .where(OrientacaoJuridica.status == True)
+            .where(Atendido.status == True)
+            .where(Atendido.nome.ilike(f"%{busca}%"))
+            .order_by(OrientacaoJuridica.id)
+        )
+        orientacoes_juridicas = create_pagination(
+            orientacoes_stmt, page_caso, app.config["CASOS_POR_PAGINA"]
+        )
+
     return render_template(
         "busca_geral.html",
         assistidos=assistidos,
         assistidos_pjuridica=assistidos_pjuridica,
         usuarios=usuarios,
         casos=casos,
+        orientacoes_juridicas=orientacoes_juridicas,
         busca_atual=busca,
     )
