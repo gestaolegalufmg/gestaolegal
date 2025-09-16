8dc425b029108427a449fe45357a375103ff984f
AndreOGodoy <godoy.andre@hotmail.com>
2025-09-16 11:41:53 -0300

    fix: fix forms imports

--- gestaolegal/forms/plantao/__init__.py
+++ gestaolegal/forms/plantao/__init__.py
@@ -3,15 +3,17 @@ from typing import Any
 from flask_wtf import FlaskForm
 from wtforms import (
     DateField,
+    FloatField,
     HiddenField,
     SubmitField,
     TimeField,
 )
 from wtforms.validators import (
     InputRequired,
+    ValidationError,
 )
 
-from .base_form_mixin import BaseFormMixin
+from gestaolegal.forms.base_form_mixin import BaseFormMixin
 
 orientacao_AdminOuCivil = {
     "ADMINISTRADOR": ("adm", "Administrador"),
@@ -24,35 +26,3 @@ MSG_SelecioneUmaOpcaoLista = "Por favor selecione uma opção de {} da lista"
 MSG_EscolhaUmaData = "Por favor, escolha uma data {}"
 
 # Field length limits
-
-
-#####################################################
-#################### FORMS ##########################
-#####################################################
-
-
-class AbrirPlantaoForm(BaseFormMixin, FlaskForm):
-    data_abertura = DateField("Data de abertura", validators=[InputRequired()])
-
-    hora_abertura = TimeField("Horário de Abertura", validators=[InputRequired()])
-
-
-class SelecionarDuracaoPlantaoForm(BaseFormMixin, FlaskForm):
-    hdnDiasEscolhidos = HiddenField("Dias escolhidos")
-    hdnDataAbertura = HiddenField("Data de abertura")
-    hdnDataFechamento = HiddenField("Data de fechamento")
-    hdnHoraAbertura = HiddenField("Hora de abertura")
-    hdnHoraFechamento = HiddenField("Hora de fechamento")
-    submit = SubmitField("Confirmar")
-
-
-class FecharPlantaoForm(BaseFormMixin, FlaskForm):
-    data_fechamento = DateField(
-        "Data de fechamento",
-        validators=[InputRequired()],
-    )
-
-    hora_fechamento = TimeField(
-        "Horário de fechamento",
-        validators=[InputRequired()],
-    )
--- gestaolegal/utils/context_processors.py
+++ gestaolegal/utils/context_processors.py
@@ -1,22 +1,15 @@
 from gestaolegal.common.constants import (
     UserRole,
-    area_atuacao,
     assistencia_jud_areas_atendidas,
     assistencia_jud_regioes,
     beneficio,
     como_conheceu_daj,
     contribuicao_inss,
-    enquadramento,
-    escolaridade,
-    estado_civilUsuario,
     moradia,
-    orgao_reg,
     participacao_renda,
     qual_pessoa_doente,
     regiao_bh,
-    sexo_usuario,
     situacao_deferimento,
-    tipo_bolsaUsuario,
     tipo_evento,
 )
 from gestaolegal.utils.color_system import generate_color_palette
@@ -45,39 +38,9 @@ def processor_tipo_classe():
     return dict(tipo_classe=tipo_classe)
 
 
-def processor_formata_float():
-    """Add formata_float function to template context."""
-
-    def formata_float(numero: float):
-        lista_char = list(str(numero))
-
-        for i in range(len(lista_char)):
-            if lista_char[i] == ".":
-                lista_char[i] = ","
-                break
-        return "".join(lista_char)
-
-    return dict(formata_float=formata_float)
-
-
 def insere_usuario_roles():
     """Add UserRole enum to template context."""
     return dict(UserRole=UserRole)
-
-
-def insere_tipo_bolsaUsuario():
-    """Add tipo_bolsaUsuario to template context."""
-    return dict(tipo_bolsaUsuario=tipo_bolsaUsuario)
-
-
-def insere_sexo_usuario():
-    """Add sexo_usuario to template context."""
-    return dict(sexo_usuario=sexo_usuario)
-
-
-def insere_estado_civilUsuario():
-    """Add estado_civilUsuario to template context."""
-    return dict(estado_civilUsuario=estado_civilUsuario)
 
 
 def insere_como_conheceu_dajUsuario():
@@ -123,26 +86,6 @@ def insere_qual_pessoa_doente():
 def insere_regiao_bh():
     """Add regiao_bh to template context."""
     return dict(regiao_bh=regiao_bh)
-
-
-def insere_escolaridade():
-    """Add escolaridade to template context."""
-    return dict(escolaridade=escolaridade)
-
-
-def insere_enquadramento():
-    """Add enquadramento to template context."""
-    return dict(enquadramento=enquadramento)
-
-
-def insere_area_atuacao():
-    """Add area_atuacao to template context."""
-    return dict(area_atuacao=area_atuacao)
-
-
-def insere_orgao_reg():
-    """Add orgao_reg to template context."""
-    return dict(orgao_reg=orgao_reg)
 
 
 def insere_situacao_deferimento():
